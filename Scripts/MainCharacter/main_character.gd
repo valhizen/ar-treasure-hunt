f213e1ea868317b4dc760b7f6ecae9904ca2cfb4
fy-st0rm <slokpradhan2062@gmail.com>
2025-12-12 00:44:36 +0545

    Initial work in lake

--- Scripts/MainCharacter/main_character.gd
+++ Scripts/MainCharacter/main_character.gd
@@ -28,19 +28,18 @@ func update_animation(direction : Vector2):
 				animated_sprite_2d.play("run_right")
 			else:
 				animated_sprite_2d.play("run_left")
-				
 		else:
 			if direction.y > 0:
 				animated_sprite_2d.play("run_down")
 			else:
 				animated_sprite_2d.play("run_up")
 	else:
-			var current_animation = animated_sprite_2d.animation
-			if "run" in current_animation:
-				var idle_dir = current_animation.replace("run", "idle")
-				animated_sprite_2d.play(idle_dir)
-			elif not "idle" in current_animation:
-				animated_sprite_2d.play("idle_down")
+		var current_animation = animated_sprite_2d.animation
+		if "run" in current_animation:
+			var idle_dir = current_animation.replace("run", "idle")
+			animated_sprite_2d.play(idle_dir)
+		elif not "idle" in current_animation:
+			animated_sprite_2d.play("idle_down")
 
 	
 func look_at_mouse():
