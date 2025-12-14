59de850255d85c8b46a3e7af3f76c4be8a721570
fy-st0rm <slokpradhan2062@gmail.com>
2025-12-14 20:47:29 +0545

    Ported PlatformerController to the MainCharacter + Added Hitting the Corrupters

--- MiniGames/Lake/water_area.gd
+++ MiniGames/Lake/water_area.gd
@@ -5,9 +5,9 @@ func _ready() -> void:
 	connect("body_exited", Callable(self, "_on_body_exited"))
 
 func _on_body_entered(body):
-	if body.name == "PlatformerCharacter":
-		body.in_water = true
+	if body.name == "MainCharacter":
+		body.just_entered_water = true
 
 func _on_body_exited(body):
-	if body.name == "PlatformerCharacter":
-		body.in_water = false
+	if body.name == "MainCharacter":
+		body.just_exited_water = true
