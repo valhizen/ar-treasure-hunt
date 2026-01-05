afb74d7bf9e17b8a181d303cbe33b5f45b86b119
Himesh Dulal <108019916+Githeimer@users.noreply.github.com>
2026-01-06 01:24:05 +0545

    bhaktapur map polished, just add minigame linkage and fix prolouge part

--- Scenes/MiniGames/Lake/bkt_lake_minigame.gd
+++ Scenes/MiniGames/Lake/bkt_lake_minigame.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready() -> void:
 	var character = get_node("MainCharacter")
-d	character.platformer = true
+	character.platformer = true
 
 func _process(delta: float) -> void:
 	pass
